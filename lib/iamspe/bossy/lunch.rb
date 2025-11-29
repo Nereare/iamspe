0f1615d33c468f774277f8684c4081e394cbc99e
Igor Padoim <igorpadoim@gmail.com>
2025-11-29 11:26:48 +0000

    Iniciar função de almoços

---
+++ lib/iamspe/bossy/lunch.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'tty-prompt'
+
+# TODO: Meio que a porra toda...
+module Iamspe
+  module Bossy
+    # Classe de geração de anúncio de almoços
+    class Lunch
+      # Inicializador
+      def initialize
+        # Inicializar TTY::Prompt
+        @prompt = TTY::Prompt.new
+        # Compilar texto de "output"
+        @out = build_lunchs
+      end
+
+      # Converter em _String_, no caso sendo o texto de "output"
+      def to_s
+        @out
+      end
+
+      private
+
+      # Construir lista de almoços
+      def build_lunchs
+        txt = @prompt.multiline('Colar (Ctrl+V) as colunas B-F do controle de salas (incluindo linhas em branco):') do |q|
+          q.help 'Use Ctrl+D quando terminar de colar'
+        end
+        txt.filter! do |elem|
+          elem ~ /(\d+|Rv)\t([A-zÇçáéíóúãõôê ]+)\t(07-19+h)\t([0-9A-z'-]+)\t(\d+h\d+)/
+        end
+        puts txt.join "\n"
+      end
+    end
+  end
+end
